e393f54ec11b0398fcb0ac96cea0d8cb231632f4
austinmr <austin.riedel@gmail.com>
2016-12-15 18:24:41 -0800

    Modify components to work with added features

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Navigation from '../containers/Navigation'
 
 const App = ({ children }) => (
     <div className="App">
+      <Navigation />
         { children }
     </div>
 );
