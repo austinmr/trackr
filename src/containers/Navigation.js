f1febad6849d784d2b9910c1775edf5eb4990b4f
austinmr <austin.riedel@gmail.com>
2016-12-15 18:36:51 -0800

    Modify styling of Navigation bar

--- src/containers/Navigation.js
+++ src/containers/Navigation.js
@@ -4,10 +4,6 @@ import { Nav, NavItem, Navbar } from 'react-bootstrap';
 
 
 class NavigationBar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   handleNavigation(event) {
     event.preventDefault();
     var route = event.target.title; 
@@ -16,21 +12,19 @@ class NavigationBar extends React.Component {
 
   render() {
     return (
-        <Navbar style={{'marginBottom': '2px', height: '40px'}}>
+        <Navbar style={{ 'marginBottom': '2px', height: '80px', background: 'black'}}>
           <Navbar.Header>
             <Navbar.Brand>
-              <a>TRACKR</a>
+              <a style={{'marginTop': '20px', 'fontSize': '40px', 'fontFamily': 'ModernSans', 'color':'white'}}>TRACKR</a>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
-          <Navbar.Collapse>
             <Nav>
-              <NavItem onClick={(e) => {this.handleNavigation(e)}} title={'/Nav1'}> Nav1 </NavItem>
-              <NavItem onClick={(e) => {this.handleNavigation(e)}} title={'/Nav2'}> Nav2 </NavItem>
-              <NavItem onClick={(e) => {this.handleNavigation(e)}} title={'/Nav3'}> Nav3 </NavItem>
-              <NavItem onClick={(e) => {this.handleNavigation(e)}} title={'/Nav4'}> Nav4 </NavItem>
+              <NavItem className="navHeader" onClick={(e) => {this.handleNavigation(e)}} title={'/Nav1'}> Nav1 </NavItem>
+              <NavItem className="navHeader" onClick={(e) => {this.handleNavigation(e)}} title={'/Nav2'}> Nav2 </NavItem>
+              <NavItem className="navHeader" onClick={(e) => {this.handleNavigation(e)}} title={'/Nav3'}> Nav3 </NavItem>
+              <NavItem className="navHeader" onClick={(e) => {this.handleNavigation(e)}} title={'/Nav4'}> Nav4 </NavItem>
             </Nav>
-          </Navbar.Collapse>
         </Navbar>
     ); 
   }
