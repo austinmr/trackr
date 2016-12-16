8953f221d3e7fa37e688c8f2c9e84609f50c23b1
austinmr <austin.riedel@gmail.com>
2016-12-15 18:30:24 -0800

    Modify actions for templates and user

--- src/actions/templates.js
+++ src/actions/templates.js
@@ -1,3 +1,4 @@
+import { CREATE_TEMPLATE, SAVE_TEMPLATE, ADD_EXERCISE, ADD_SET } from '../constants/ActionTypes'
 import { v4 } from 'uuid'
 
 const createDate = () => {
@@ -7,9 +8,34 @@ const createDate = () => {
 
 export const createTemplate = (username) => {
   return {
-    type: 'CREATE_TEMPLATE',
+    type: CREATE_TEMPLATE,
     id: v4(),
     date: createDate(),
     username,
+  }
+}
+
+export const saveTemplate = (template) => {
+  return {
+    type: SAVE_TEMPLATE,
+    id: template.id,
+    template,
+  }
+}
+
+export const addExercise = (id, exercise) => {
+  return {
+    type: ADD_EXERCISE,
+    id, 
+    exercise,
+  }
+}
+
+export const addSet = (exerciseId, reps) => {
+  return {
+    type: ADD_SET,
+    id: v4(),
+    exerciseId,
+    reps
   }
 }
--- src/actions/user.js
+++ src/actions/user.js
@@ -1,9 +1,11 @@
+import { LOGIN_USER } from '../constants/ActionTypes'
 import { v4 } from 'uuid'
 
 export const loginUser = (username) => {
   return {
-    type: 'LOGIN_USER',
+    type: LOGIN_USER,
     id: v4(),
     username
   }
 }
+
