8a42edf134f8b1a59ba9ee336ca9e3df6576636e
austinmr <austin.riedel@gmail.com>
2016-12-15 18:38:54 -0800

    Modify routes

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,15 @@ import { Route, IndexRoute } from 'react-router'
 import App from './components/App'
 import LandingPage from './containers/LandingPage'
 import UserProfile from './containers/UserProfile'
-
+import Template from './containers/Template'
+import Workout from './containers/Workout'
 
 export default <Route path="/" component={App}>
   <IndexRoute component={LandingPage} />
   <Route path="/User(/:username)"
          component={UserProfile} />
+  <Route path="/Template(/:username)"
+         component={Template} />
+  <Route path="/Workout(/:username)"
+         component={Workout} />
 </Route>
