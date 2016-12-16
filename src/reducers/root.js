fe9302f3073f35376a8ad949f7e5ad570cd4de25
austinmr <austin.riedel@gmail.com>
2016-12-15 18:33:12 -0800

    Modify root reducer to make use of added userExercise, results and other reducers

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -1,12 +1,32 @@
 import { combineReducers } from 'redux'
 import { routerReducer as routing } from 'react-router-redux'
 import user from './user'
-import templates from './templates'
+import userExercises, * as fromUserExercises from './userExercises'
+import template from './templates'
+import workout from './workouts'
+import results from './results'
+import entities, * as fromEntities from './entities'
 
 const app = combineReducers({
   routing,
   user,
-  templates,
+  template,
+  workout,
+  entities,
+  userExercises,
+  results
 })
 
 export default app
+
+export const getExercisesFromTemplate = (state) => 
+  fromEntities.getExercisesFromTemplate(state.entities, state.workout.template)
+
+export const getTemplatesObjectArray = (state) => 
+  fromEntities.getTemplatesObjectArray(state.entities)
+
+export const getUserExercisesInWorkout = (state) => 
+  fromUserExercises.getUserExercisesInWorkout(state.userExercises, state.workout.exercises)
+
+export const getUserExercisesInWorkoutMiddleware = (state, exercises) => 
+  fromUserExercises.getUserExercisesInWorkout(state.userExercises, exercises)
