ab20ade673ae08f2bf375f77647f2ff515486fa5
austinmr <austin.riedel@gmail.com>
2016-12-15 18:36:05 -0800

    Modify dependencies and public files to support changes

--- scripts/test.js
+++ scripts/test.js
@@ -11,8 +11,8 @@ const jest = require('jest');
 const argv = process.argv.slice(2);
 
 // Watch unless on CI
-if (!process.env.CI) {
-  argv.push('--watch');
-}
+// if (!process.env.CI) {
+//   argv.push('--watch');
+// }
 
 jest.run(argv);
