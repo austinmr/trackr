1b2484f2960559cab08fad7f309f54dfb2c14577
austinmr <austin.riedel@gmail.com>
2016-12-15 18:37:10 -0800

    Modify UserProfile containers to utilize redux middleware for generating new user workouts based on DynamoDB data for performance stats and workout templates.

--- src/containers/UserProfile.js
+++ src/containers/UserProfile.js
@@ -3,9 +3,12 @@ import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { browserHistory } from 'react-router';
 import { createTemplate } from '../actions/templates'
+import { fetchUserExercisesIfNeeded } from '../actions/userExercises'
+import { createWorkoutFromTemplateMiddleware } from '../actions/workouts'
+import { getTemplatesObjectArray } from '../reducers/root'
 
 // App Components 
-// import WorkoutEntry from '../components/WorkoutEntry'
+import TemplateEntry from '../components/TemplateEntry'
 
 // Bootstrap Imports 
 import { Grid, Row, Col, Button, Nav, NavItem, Badge } from 'react-bootstrap';
@@ -14,6 +17,16 @@ export class UserProfile extends React.Component {
   static propTypes = {
     username: PropTypes.string.isRequired, 
     dispatchCreateTemplate: PropTypes.func.isRequired, 
+  }
+
+  state = {
+    activeKey: 2
+  }
+
+  constructor(props) {
+    super(props);
+    const { userID, dispatchFetchUserExercises } = this.props; 
+    dispatchFetchUserExercises(userID); 
   }
 
   handleCreateTemplate = (username) => {
@@ -25,37 +38,48 @@ export class UserProfile extends React.Component {
     }
   }
 
-  // handleTabSelect(eventKey) {
-  //   this.setState({activeKey: eventKey}); 
-  // }
+  handleCreateWorkout = (template) => {
+    const { userID, username, dispatchCreateWorkout } = this.props; 
+    const exercises = template.exercises.map((exercise) => exercise.id);
+    dispatchCreateWorkout(userID, username, template, exercises);
 
-  // _renderNavigationBar(){
-  //   return (
-  //     <Nav bsStyle="tabs" justified onSelect={this.handleTabSelect.bind(this)} style={{marginTop: 20}}>
-  //       <NavItem eventKey={1} title="Workouts">Workouts</NavItem>
-  //       <NavItem eventKey={2} title="Templates">Templates</NavItem>
-  //       <NavItem eventKey={3} title="Following" disabled>Friends</NavItem>
-  //       <NavItem eventKey={4} title="Achievements" disabled>Achievements <Badge>{Math.floor(Math.random() * 100)}</Badge></NavItem>
-  //     </Nav>
-  //   )
-  // }
+    // Prevent 'SecurityError' message from Jest 
+    if (process.env.NODE_ENV !== 'test') {
+      browserHistory.push(`/Workout/${username}`);
+    }
+  }
 
-  // _renderActiveComponent(){
-  //   const { workouts, templates, viewWorkout } = this.props; 
-  //   if (this.state.activeKey === 1) {
-  //     return (
-  //       workouts.map((workout, i) => (
-  //         <WorkoutEntry key={workout.id} workout={workout} onClick={() => {viewWorkout(workout)}} />
-  //       ))
-  //     )
-  //   } else if (this.state.activeKey === 2) {
-  //     return (
-  //       templates.map((template, i) => (
-  //         <WorkoutEntry key={template.id} workout={template} onClick={() => {viewWorkout(template)}} />
-  //       ))
-  //     )
-  //   }
-  // }
+  handleTabSelect(eventKey) {
+    this.setState({activeKey: eventKey}); 
+  }
+
+  _renderNavigationBar(){
+    return (
+      <Nav bsStyle="tabs" justified onSelect={this.handleTabSelect.bind(this)} style={{marginTop: 20}}>
+        <NavItem eventKey={1} title="Workouts" disabled>Workouts</NavItem>
+        <NavItem eventKey={2} title="Templates">Templates</NavItem>
+        <NavItem eventKey={3} title="Following" disabled>Friends</NavItem>
+        <NavItem eventKey={4} title="Achievements" disabled>Achievements <Badge>{Math.floor(Math.random() * 100)}</Badge></NavItem>
+      </Nav>
+    )
+  }
+
+  _renderActiveComponent(){
+    const { templates } = this.props; 
+    if (this.state.activeKey === 1) {
+      // return (
+      //   // workouts.map((workout, i) => (
+      //   //   <WorkoutEntry key={workout.id} workout={workout} onClick={() => {viewWorkout(workout)}} />
+      //   // ))
+      // )
+    } else if (this.state.activeKey === 2) {
+      return (
+        templates.map((template, i) => (
+          <TemplateEntry key={template.id} onClick={()=>{this.handleCreateWorkout(template)}} {...template}/>
+        ))
+      )
+    }
+  }
 
   render() {
     const { username } = this.props; 
@@ -71,6 +95,8 @@ export class UserProfile extends React.Component {
             <Button className="template-button" bsSize="large" onClick={(e)=>{this.handleCreateTemplate(username)}}> New Workout Template </Button> 
           </Col> 
           <Col xs={8} md={8}>
+            {this._renderNavigationBar()}
+            {this._renderActiveComponent()}
           </Col>
         </Row> 
       </Grid> 
@@ -78,21 +104,26 @@ export class UserProfile extends React.Component {
   }
 }
 
-const mapStateToProps = (state, ownProps) => ({
+const mapStateToProps = (state, { params }) => ({
+  userID: state.user.id, 
   username: state.user.username,
   workouts: state.user.workouts,
-  templates: state.user.templates,
+  templates: getTemplatesObjectArray(state, params.username),
+  userExercises: state.userExercises.exercises
 })
 
 const mapDispatchToProps = (dispatch) => ({
   dispatchCreateTemplate: (username) => {
     dispatch(createTemplate(username))
   },
+  dispatchCreateWorkout: (userID, username, template, userExercises) => {
+    dispatch(createWorkoutFromTemplateMiddleware(userID, username, template, userExercises))
+  },
+  dispatchFetchUserExercises: (id) => {
+    dispatch(fetchUserExercisesIfNeeded(id))
+  },
 }) 
 
 const UserProfileContainer = connect(mapStateToProps, mapDispatchToProps)(UserProfile)
 
 export default UserProfileContainer 
-
-// {this._renderNavigationBar()}
-// {this._renderActiveComponent()}
